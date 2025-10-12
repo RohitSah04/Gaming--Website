839df8c227700577cff053b549e9099c1e145464
RohitSah04 <rohitsumit0231@gmail.com>
2025-10-12 23:07:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const video = document.querySelector('.hero-video');
 
 const movieList = ['videos/hero-1.mp4','videos/hero-2.mp4','videos/hero-3.mp4','videos/hero-4.mp4'];
 
-let index = 0;
+let index =  0;
 nextButton.addEventListener('click', function(){
 
     index += 1
