18a6a72e8f965e4663cc4289525f5a1c0eb31b19
RohitSah04 <rohitsumit0231@gmail.com>
2026-02-16 10:17:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const movieList = ['videos/hero-1.mp4','videos/hero-2.mp4','videos/hero-3.mp4','
 
 let index =  0;
 nextButton.addEventListener('click', function(){
-
+    
     index += 1
     video.src = movieList[index];
 
